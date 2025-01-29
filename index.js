c4d294f590b15551e88bf28de76e1854726038f6
JavCan <167463904+JavCan@users.noreply.github.com>
2025-01-29 15:22:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
         // Eventos para móvil
-    container.addEventListener("touchend", function() {
+    container.addEventListener("touchstart", function() {
         card.style.transform = "translateY(-90px)";
     });
 
-    container.addEventListener("touchstart", function() {
+    container.addEventListener("touchend", function() {
         card.style.transform = "translateY(0)";
     });
 
